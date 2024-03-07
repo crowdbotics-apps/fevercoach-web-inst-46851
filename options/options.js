ceab7cd621a77344b3080353900992b8d18239ad
Crowdbotics <team@crowdbotics.com>
2024-03-07 04:03:34 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "fevercoach-web-inst-46851",
+  url: "https://fevercoach-web-inst-46851.botics.co",
+  api: "https://fevercoach-web-inst-46851.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
